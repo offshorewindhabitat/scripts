3320be9a933193fd936b6b15397f4e0a006b9ca0
Ben Best <ben@ecoquants.com>
2023-04-19 10:01:18 -0400

    ecoquants/offhabr -> offshorewindhabitat/offhabr

--- sp_map/global.R
+++ sp_map/global.R
@@ -1,25 +1,12 @@
-# old packages for dev:
-# librarian::shelf(
-#   dplyr, fs, leaflet,
-#   ecoquants/offhabr,
-#   shiny)
-# devtools::install_local(here::here("../offhabr"), force = T)
-# devtools::load_all(here::here("../offhabr"))
-
-# packages for shinyapps.io
-# remotes::install_github("ecoquants/offhabr", force = T)
+# R packages (to deploy on ShinyApps.io)
 library(dplyr)
 library(fs)
 library(leaflet)
 library(leaflet.extras)
-library(offhabr)
+library(offhabr)  # remotes::install_github("offshorewindhabitat/offhabr", force = T)
 library(shiny)
 
-# devtools::session_info()
-# offhabr             * 0.11.0     2023-04-14 [1] Github (ecoquants/offhabr@4ed52d4)
-# leaflet             * 2.1.1.9000 2023-01-02 [1] Github (rstudio/leaflet@51daa69)
-
-con <- oh_con() # dbDisconnect(con, shutdown=TRUE)
+con <- oh_con()   # dbDisconnect(con, shutdown=TRUE)
 
 d_spp <- tbl(con, "lyrs") |>
   filter(
@@ -36,5 +23,7 @@ d_spp <- tbl(con, "lyrs") |>
       kingdom, phylum, class, order,family, scientificname) |>
       paste(collapse = " > ")) |>
   arrange(taxa_hierarchy)
-# TODO: taxa_wm: update class; + common_name
-# TODO: ds_key per layer
+
+# TODO:
+# - taxa_wm: update class; + common_name
+# - ds_key per layer
